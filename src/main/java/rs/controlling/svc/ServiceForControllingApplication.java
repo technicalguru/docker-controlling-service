0cd86bd23a967df5a523fb3df81973224c1fd08c
technicalguru <github@ralph-schuster.eu>
2021-06-14 07:49:35 +0200

    Spring Boot Skeleton

---
+++ src/main/java/rs/controlling/svc/ServiceForControllingApplication.java
@@ -0,0 +1,13 @@
+package rs.controlling.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceForControllingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceForControllingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/controlling/svc/ServiceForControllingApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.controlling.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceForControllingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
